2dec44445db7dad99ed5092db48c2e2402147e93
Charles Bréton <charlesbreton@outlook.com>
2024-02-16 05:38:22 +0100

    hashtag + route

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,10 +1,29 @@
 import styles from '../styles/Tweet.module.css';
 import {useState} from 'react';
+import { useSelector } from 'react-redux';
 
 function Tweet() {
 
-  const [tweetLength, setTweetLength] = useState(0);
+  const [tweetText, setTweetLength] = useState('');
+  const user = useSelector((state) => state.user.value);
 
+  const pattern = /#\w+/gi;
+  const hashtags = tweetText.match(pattern);
+
+  const handleTweet = () => {
+    fetch('http://localhost:3000/tweets', {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        text: tweetText,
+        date: new Date(),
+        hashtag: hashtags,
+        author: user.token,
+      })
+    })
+    .then(response => response.json())
+    .then(data => {})
+  }
 
   return (
     <div>
@@ -13,10 +32,10 @@ function Tweet() {
          Home
         </h1>
         <div className={styles.centre}>
-          <textarea type="text" onChange={(e) => {setTweetLength(e.target.value)}} value={tweetLength} className={styles.input} placeholder='Tweet here ...' maxLength="280" ></textarea>
+          <textarea type="text" onChange={(e) => {setTweetLength(e.target.value)}} value={tweetText} className={styles.input} placeholder="What's up ..." maxLength="280" ></textarea>
           <div className={styles.compteur}>
-            <p> {tweetLength.length} / 280 </p>
-            <button className={styles.button}>Tweet</button>
+            <p> {tweetText.length} / 280 </p>
+            <button onClick={()=> handleTweet()} className={styles.button}>Tweet</button>
           </div>
         </div>
       </main>
